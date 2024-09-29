59b0d35783e0788d761e55bef4e2cb44e7f4cf8b
Pourya Samimi <neverdiegenius@gmail.com>
2024-09-29 16:13:04 -0700

    file

--- app/inlineButton/button.go
+++ app/inlineButton/button.go
@@ -7,7 +7,6 @@ import (
 
 	"github.com/itpourya/Haze/app/entity"
 	"github.com/itpourya/Haze/app/serializer"
-	"gopkg.in/telebot.v3"
 )
 
 func Start() (string, *telebot.ReplyMarkup) {
@@ -143,7 +142,7 @@ func Send() string {
 func Me(userData serializer.Response) (string, *telebot.ReplyMarkup) {
 	status := userData.Status
 	configName := userData.Username
-	link := "https://marz.ikernel.ir:8000" + userData.SubscriptionURL
+	link := "https://marz.redzedshop.ir:8000" + userData.SubscriptionURL
 	buyTime := strings.Replace(userData.CreatedAt[0:10], "-", "/", 2)
 	expire := validateTime(userData.Expire)
 	dataLimit := validateDataLimit(userData.DataLimit)

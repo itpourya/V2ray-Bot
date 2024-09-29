d186d4f2d218fc021378323eeb555d35ee02be72
Pourya Samimi <neverdiegenius@gmail.com>
2024-09-29 15:34:26 -0700

    file

--- app/cache/redis.go
+++ app/cache/redis.go
@@ -1,12 +1,8 @@
 package cache
 
-import (
-	"github.com/redis/go-redis/v9"
-)
-
 func NewCache() *redis.Client {
 	rdb := redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
+		Addr:     "haze_cache:6379",
 		Password: "eYVX7EwVmmxKPCDmwMtyKVge8oLd2t81",
 		DB:       0, // use default DB
 	})
